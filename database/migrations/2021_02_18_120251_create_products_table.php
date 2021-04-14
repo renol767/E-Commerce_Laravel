48a3bcc24e21199f4bcfc4a3b5f5c785ffd8f20f
renol767 <72129061+renol767@users.noreply.github.com>
2021-04-15 04:16:11 +0700

    firstcommit baru sampai admin page

---
+++ database/migrations/2021_02_18_120251_create_products_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            // sku,name,price,weight,width,height,depth,slug,short_description,description,status
+            $table->string('sku');
+            $table->string('name');
+            $table->string('slug');
+            $table->decimal('price', 15, 2);
+            $table->decimal('weight', 10, 2);
+            $table->decimal('width', 10, 2);
+            $table->decimal('height', 10, 2);
+            $table->decimal('depth', 10, 2);
+            $table->text('short_description');
+            $table->text('description');
+            $table->integer('status');
+            $table->timestamps();
+
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
